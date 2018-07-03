a660acdc733c16d245c56cf1a5c6cf8511ce632c
Brandon Chong <brndnchong@gmail.com>
2018-07-03 09:44:48 -0700

    Add flash cards

--- scripts/dinos.js
+++ scripts/dinos.js
@@ -2,32 +2,73 @@ const dinosaurElem = document.getElementById("dinosaur")
 const classificationElem = document.getElementById("classification")
 const locationElem = document.getElementById("location")
 const ageElem = document.getElementById("age")
+const notesElem = document.getElementById("notes")
 
-fetch('../data/dinos.json')
-  .then(response => response.text())
-  .then(text => load(text))
+let slideIdx = 0;
+let revealed = false;
+let dinolist = theropods;
+dinolist.sort( () => Math.random() - 0.5)
 
-let dinos;
-let theropods;
-let slideIdx = -1;
-
-function load(json) {
-    dinos = JSON.parse(json);
-    theropods = dinos.theropods;
-    theropods.sort( () => Math.random() - 0.5)
-}
-
-function nextSlide() {
-    slideIdx++;
-
-    dino = theropods[slideIdx];
+function showSlide(index) {
+    dino = dinolist[index];
 
     dinosaurElem.textContent = dino.dinosaur;
     classificationElem.textContent = "";
     locationElem.textContent = "";
-    ageElem.textContent = ""
+    ageElem.textContent = "";
+    while (notesElem.firstChild) {
+        notesElem.firstChild.remove();
+    }
+
+    revealed = false;
+}
+
+function reveal() {
+    dino = dinolist[slideIdx];
+    classificationElem.textContent = dino.classification;
+    locationElem.textContent = dino.location;
+    ageElem.textContent = dino.age;
+
+    if (dino.notes.length == 0) {
+        notesElem.textContent = "(none)";
+    }
+
+    for (let note of dino.notes) {
+        var li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.textContent = note;
+        notesElem.appendChild(li);
+    }
+
+    revealed = true;
 }
 
 document.getElementById("next").onclick = ev => {
-    nextSlide();
+    slideIdx++;
+    if (slideIdx >= dinolist.length) {
+        slideIdx = 0;
+    }
+
+    showSlide(slideIdx);
 }
+
+document.getElementById("previous").onclick = ev => {
+    slideIdx--;
+    if (slideIdx < 0) {
+        slideIdx = dinolist.length - 1;
+    }
+
+    showSlide(slideIdx);
+}
+
+document.getElementById("dino-container").onclick = ev => {
+    if (revealed) {
+        return;
+    }
+
+    reveal();
+};
+
+showSlide(slideIdx);
+
+console.log(dinolist);
---
+++ scripts/theropods.js
@@ -0,0 +1,406 @@
+const theropods = [
+    {
+        "dinosaur": "Dilophosaurus (two-crested lizard)",
+        "classification": "Early Theorpod",
+        "location": "North America",
+        "age": "Early Jurassic, 193 Ma",
+        "notes": [
+            "First large theropod to be an apex predator"
+        ]
+    },
+    {
+        "dinosaur": "Ceratosaurus (horned lizard)",
+        "classification": "Ceratosaur",
+        "location": "North America, Portugal",
+        "age": "Late Jurassic, 150 Ma",
+        "notes": [
+            "Elongate nasal horn",
+            "Pair of hornlets over the eyes",
+            "Small row of osteoderms along its back"
+        ]
+    },
+    {
+        "dinosaur": "Carnotaurus (meat-eating bull)",
+        "classification": "Abelisaur",
+        "location": "South America",
+        "age": "Late Cretaceous, 72 Ma",
+        "notes": [
+            "Bull-like horns",
+            "Known from one well-preserved skeleton from Argentina"
+        ]
+    },
+    {
+        "dinosaur": "Megalosaurus (great lizard)",
+        "classification": "Megalosaur",
+        "location": "England",
+        "age": "Middle Jurassic, 166 Ma",
+        "notes": [
+            "First giant reptile discovered and named"
+        ]
+    },
+    {
+        "dinosaur": "Spinosaurus (spine lizard)",
+        "classification": "Spinosaur",
+        "location": "North Africa",
+        "age": "Late Cretaceous, 95 Ma",
+        "notes": [
+            "Largest spinosaur and theropod (up to 17m in length)",
+            "Originally discovered in Egypt in the 1900s and fossil destroyed in World War 2 bombings"
+        ]
+    },
+    {
+        "dinosaur": "Baryonyx (heavy claw)",
+        "classification": "Spinosaur",
+        "location": "England",
+        "age": "Early Cretaceous, 130 Ma",
+        "notes": [
+            "First fish eating dinosaur discovered with fish scale in stomach",
+            "Long straight neck",
+            "Spoon shaped snout",
+            "Large first finger claw"
+        ]
+    },
+    {
+        "dinosaur": "Suchomimus (crocodile mimic)",
+        "classification": "Spinosaur",
+        "location": "Africa",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "Crocodile-like skull"
+        ]
+    },
+    {
+        "dinosaur": "Irratator",
+        "classification": "Spinosaur",
+        "location": "South America",
+        "age": "Early Cretaceous, 110 Ma",
+        "notes": [
+        ]
+    },
+    {
+        "dinosaur": "Allosaurus (different lizard)",
+        "classification": "Allosaur",
+        "location": "North America",
+        "age": "Late Jurassic, 150 Ma",
+        "notes": [
+            "Apex predator preying on Stegosaurus, ornithopods and sauropods",
+            "Fossils come from the Morrison Formation",
+            "'Big Al' specimen is 95% complete and partly articulated"
+        ]
+    },
+    {
+        "dinosaur": "Carcharodontosaurus (sharp tooth lizard)",
+        "classification": "Allosaur",
+        "location": "Northern Africa",
+        "age": "Late Cretaceous, 90 Ma",
+        "notes": [
+            "Shark-like teeth"
+        ]
+    },
+    {
+        "dinosaur": "Giganotosaurus (giant southern lizard)",
+        "classification": "Allosaur",
+        "location": "Argentina",
+        "age": "Late Cretaceous, 87 Ma",
+        "notes": [
+            "Teeth for cutting flesh instead of crushing"
+        ]
+    },
+    {
+        "dinosaur": "Guanlong (crowned dragon)",
+        "classification": "Tyrannosaur",
+        "location": "Northern China",
+        "age": "Middle Jurassic",
+        "notes": [
+            "Known from 2 specimens caught in a mud pit with 3 ceratosaurs",
+            "Smaller and lighting than later tyrannosaurids"
+        ]
+    },
+    {
+        "dinosaur": "Dilong (emperor's dragon)",
+        "classification": "Tyrannosaur",
+        "location": "China",
+        "age": "Early Cretaceous, 126 Ma",
+        "notes": [
+            "Proto feathers"
+        ]
+    },
+    {
+        "dinosaur": "Yutyrannus (feathered tyrant",
+        "classification": "Tyrannosaur",
+        "location": "China",
+        "age": "Early Cretaceous",
+        "notes": [
+            "Largest dinosaur with evidence of feathers",
+            "Specimens show proto-feathers in patches = fuzzy dinosaur"
+        ]
+    },
+    {
+        "dinosaur": "Albertosaurus (alberta lizard",
+        "classification": "Tyrannosaur",
+        "location": "Western North America",
+        "age": "Late Cretaceous, 70 Ma",
+        "notes": [
+            "Less robust than other tyrannosaurs",
+            "Most work done by Phil Currie",
+            "More agile"
+        ]
+    },
+    {
+        "dinosaur": "Gorgosaurus (dreadful lizard)",
+        "classification": "Tyrannosaur",
+        "location": "Western North America",
+        "age": "Late Cretaceous, 75 Ma",
+        "notes": [
+            "Less robust than other tyrannosaurs",
+            "Some feathers on body"
+        ]
+    },
+    {
+        "dinosaur": "Daspletosaurus (frightful lizard)",
+        "classification": "Tyrannosaur",
+        "location": "Western North America",
+        "age": "Late Cretaceous, 75 Ma",
+        "notes": [
+            "Larger and more robust tyrannosaur",
+            "Apex predator specialized in hunting ceratopsids and akylosaurs"
+        ]
+    },
+    {
+        "dinosaur": "Tarbosaurus (alarming lizard)",
+        "classification": "Tyrannosaur",
+        "location": "Gobi Desert",
+        "age": "Late Cretaceous 70 Ma",
+        "notes": [
+            "Slightly smaller, Asian cousin of T-Rex",
+            "Smallest forelimbs of tyrannosaurs"
+        ]
+    },
+    {
+        "dinosaur": "Tyrannosaurus Rex (tyrant lizard king)",
+        "classification": "Tyrannosaur",
+        "location": "North America",
+        "age": "Late Cretaceous, 68 Ma",
+        "notes": [
+            "Largest bite force of terrestrial",
+            "Adult mouth is the size of a bathtub",
+            "Known from 50+ specimens",
+            "'Black Beauty' Alberta specimen with black mineralization of the bones",
+            "'Sue' biggest, 2nd oldest and most complete (90%) specimen. Missing her left foot, right arm, vertebrae and ribs",
+            "'Trix' oldest with an estimated age of 30",
+            "'B-rex' only specimen with gender (female) determined from preserved medullary tissue",
+            "Fine scales on the body"
+        ]
+    },
+    {
+        "dinosaur": "Compsognathus (elegant jaw)",
+        "classification": "Compsognathid",
+        "location": "Europe",
+        "age": "Late Jurassic, 150 Ma",
+        "notes": [
+            "Ate small lizards"
+        ]
+    },
+    {
+        "dinosaur": "Sinosauropteryx (chinese lizard wing",
+        "classification": "Compsognathis",
+        "location": "China (Yixing formation)",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "First dinosaur found with evidence of feathers",
+            "Longest tail per body length",
+            "First dinosaur with colourization determined through preserved melanosomes"
+        ]
+    },
+    {
+        "dinosaur": "Struthiomimus (ostrich mimic)",
+        "classification": "Ornithomimaosaur",
+        "location": "Alberta",
+        "age": "Late Cretaceous, 70 Ma",
+        "notes": [
+            "Common in Alberta (Dinosaur Park and Drumheller)"
+        ]
+    },
+    {
+        "dinosaur": "Ornithomimus (bird mimic)",
+        "classification": "Ornithomimosaur",
+        "location": "North America",
+        "age": "Late Cretaceous",
+        "notes": [
+            "Most common small dinosaur found",
+            "First evidence of feathered dinosaur in North America, discovered by François Therrien"
+        ]
+    },
+    {
+        "dinosaur": "Gallimimus (chicken mimic)",
+        "classification": "Ornithomimosaur",
+        "location": "Mongolia",
+        "age": "Late Cretaceous, 70 Ma",
+        "notes": [
+            "Largest ornithomimosaur",
+            "Name referes to similarities in its neck vertebrae with chicken"
+        ]
+    },
+    {
+        "dinosaur": "Anzu",
+        "classification": "Oviraptorosaur",
+        "location": "North America",
+        "age": "Late Cretaceous, 66 Ma",
+        "notes": [
+            "Nicknamed 'Chicken from hell', from specimens found in the Hell Creek formation",
+            "Crest on its head"
+        ]
+    },
+    {
+        "dinosaur": "Caudipteryx (tail feather)",
+        "classification": "Oviraptorosaur",
+        "location": "China (Yixian formation)",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "Feathered",
+            "Herbivore or omnivore, gastroliths found in specimen"
+        ]
+    },
+    {
+        "dinosaur": "Citipati (funeral pyre lord)",
+        "classification": "Oviraptorosaur",
+        "location": "Gobi Desert",
+        "age": "Late Cretaceous, 80 Ma",
+        "notes": [
+            "Emu sized",
+            "'Big Momma' specimen found in brooding position"
+        ]
+    },
+    {
+        "dinosaur": "Macroelongatoolithus",
+        "classification": "Oviraptorosaur",
+        "location": "North America, Asia",
+        "age": "",
+        "notes": [
+            "Oogenus"
+        ]
+    },
+    {
+        "dinosaur": "Beibeilong Sinesis (baby dragon from china)",
+        "classification": "Oviraptorosaur",
+        "location": "China",
+        "age": "Late Cretaceous",
+        "notes": [
+            "Giant ovirptorosaur discovered from 'Baby Louie' embryo"
+        ]
+    },
+    {
+        "dinosaur": "Gigatoraptor",
+        "classification": "Oviraptorosaur",
+        "location": "Mongolia",
+        "age": "Late Cretacous, 85 Ma",
+        "notes": [
+            "Largest oviraptorosaur",
+            "Known from a partial specimen",
+            "Toothless"
+        ]
+    },
+    {
+        "dinosaur": "Microraptor (small thief)",
+        "classification": "Dromaeosaur",
+        "location": "China",
+        "age": "Early Cretaceous, 120 Ma",
+        "notes": [
+            "4-winged, efficient glider",
+            "Melanosomes indicate iridescent black colouration",
+            "First discovered by Xu Xing"
+        ]
+    },
+    {
+        "dinosaur": "Velociraptor (swift plunderer)",
+        "classification": "Dromaeosaur",
+        "location": "Mongolia",
+        "age": "Late Cretaceous, 75 Ma",
+        "notes": [
+            "Originally had its sickle claw misidentified as a hand claw",
+            "Quill knobs along its ulna",
+            "'Fighting Dinosaurs' with a protoceratops"
+        ]
+    },
+    {
+        "dinosaur": "Deinonychus (terrible claw)",
+        "classification": "Dromaeosaur",
+        "location": "North America",
+        "age": "Early Cretaceous, 115 Ma",
+        "notes": [
+            "First discovered in Cloverly Formation by John Ostrom of a pack of 4 surrounding a hadrosaur",
+            "Started the Dinosaur Renaissance after being described as extremely agile"
+        ]
+    },
+    {
+        "dinosaur": "Beipiaosaurus (Beipiao lizard)",
+        "classification": "Therizinosaur",
+        "location": "China (Yixian formation)",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "Feather coat with 2 layers: a cover of downy feathers and a long shaggy coat"
+        ]
+    },
+    {
+        "dinosaur": "Nothronychus (sloth claw)",
+        "classification": "Therizinosaur",
+        "location": "North America",
+        "age": "Late Cretaceous, 92 Ma",
+        "notes": [
+        ]
+    },
+    {
+        "dinosaur": "Therizinosaurus",
+        "classification": "Therizinosaur",
+        "location": "Mongolia",
+        "age": "Late Cretaceous, 70 Ma",
+        "notes": [
+            "Largest therizinosaur",
+            "Curved claws almost 1 metre in length",
+            "May have used claws to hook branches"
+        ]
+    },
+    {
+        "dinosaur": "Mei long (sleeping dragon)",
+        "classification": "Troodontids",
+        "location": "China",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "Specimen found in roosting positions with head tucked under its wings"
+        ]
+    },
+    {
+        "dinosaur": "Troodon (wounding tooth)",
+        "classification": "Troodontid",
+        "location": "North America",
+        "age": "Late Cretaceous, 77 Ma",
+        "notes": [
+            "Long serrated teeth",
+            "Laid clutches with 16 to 24 eggs",
+            "Alaskan troodons were larger"
+        ]
+    },
+    {
+        "dinosaur": "Arcaeopteryx (ancient wing)",
+        "classification": "Avialae",
+        "location": "Germany",
+        "age": "Late Jurassic, 150 Ma",
+        "notes": [
+            "'Berlin Specimen' the most complete",
+            "Transitional fossil between non-avian dinosaurs and birds",
+            "Fused fingers like modern birds",
+            "Backwards directed pubis and stiffened tail like paraves"
+        ]
+    },
+    {
+        "dinosaur": "Confuciusornis ",
+        "classification": "Avialae",
+        "location": "China (yixian formation)",
+        "age": "Early Cretaceous, 125 Ma",
+        "notes": [
+            "Link between archaic birds and modern birds",
+            "First known bird with a pygostyle",
+            "Small about the size of a pigeon"
+        ]
+    }
+]
